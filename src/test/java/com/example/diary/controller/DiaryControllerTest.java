15965c141593427cbe59095568e62a026fe50353
tmdwls2805 <qkrtmdwls280@gmail.com>
2025-11-12 12:18:21 +0900

    fix: 버전 수정

--- src/test/java/com/example/diary/controller/DiaryControllerTest.java
+++ src/test/java/com/example/diary/controller/DiaryControllerTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureTestMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureTestMvc
+@AutoConfigureMockMvc
 @Transactional
 @ActiveProfiles("test")
 class DiaryControllerTest {
